ebf57cdb7f11c1450500d9ed5939ab5f20c54a40
Ahtasham-ul-haq <ahtasham1802@gmail.com>
2025-11-21 16:47:30 +0500

    Added showFuction

--- main.cpp
+++ main.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
-#include <windows.h> // for colors
-#include <cstdlib> // for clearing screen
-#include <conio.h> //for getch();
+#include <windows.h>   // for colors
+#include <cstdlib>     // for clearing screen
+#include <conio.h>     // for getch()
 using namespace std;
 
-void setColor(int textColor, int bgColor);
-void initializeBoard(char board[10][10]);
-void clearScreen();
-
-int main()
-{
-	cout << "    ==================================\n\n";
-	cout << "          WELCOME TO BATTLESHIP GAME        ";
-	cout << "\n\n    ==================================";
-	cout << "\n\n\n              MAIN MENU\n    ----------------------------------\n";
-	char board[10][10];
-	initializeBoard(board);
-	
-	return 0;
-}
-
-// Color codes
+// ---------- Color Codes ----------
 const int BLACK = 0;
 const int BLUE = 1;
 const int GREEN = 2;
@@ -29,139 +13,114 @@ const int RED = 4;
 const int MAGENTA = 5;
 const int YELLOW = 6;
 const int WHITE = 7;
-// Function to set console text and background color
+
+// ---------- Function Declarations ----------
+void setColor(int textColor, int bgColor);
+void clearScreen();
+void initializeBoard(char board[10][10]);
+void printBoard(char board[10][10]);
+int showMenu();
+
+// ---------- MAIN PROGRAM ----------
+int main() {
+    cout << "    ==================================\n\n";
+    cout << "          WELCOME TO BATTLESHIP GAME        ";
+    cout << "\n\n    ==================================\n";
+
+    char board[10][10];
+    initializeBoard(board);
+
+    while (true) {
+        int choice = showMenu();
+        clearScreen();
+
+        if (choice == 1) {
+            cout << "\nStarting game...\n";
+            // game logic will come later...
+        }
+        else if (choice == 2) {
+            cout << "\nInstructions:\n";
+            cout << "• Place your ships on the board.\n";
+            cout << "• Try to hit enemy ships.\n";
+            cout << "• X = Hit, O = Miss, ~ = Water.\n";
+        }
+        else if (choice == 3) {
+            printBoard(board);
+        }
+        else if (choice == 4) {
+            cout << "Thanks for playing!\n";
+            break;
+        }
+
+        cout << "\nPress any key to return to menu...";
+        getch();
+        clearScreen();
+    }
+
+    return 0;
+}
+
+// ---------- FUNCTIONS ----------
+
+// Set console color
 void setColor(int textColor, int bgColor) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, (bgColor << 4) | textColor);
 }
 
+// Clear screen
 void clearScreen() {
-//    system("clear");  //For Mac /Linux;
-		system("CLS"); //For Windows
-
+    system("CLS");
 }
 
-void initializeBoard(char board[10][10]){
-//	char board[10][10];
-
-    // Initialize 10x10 board with water '~'
+// Fill the board with '~'
+void initializeBoard(char board[10][10]) {
     for (int i = 0; i < 10; i++)
         for (int j = 0; j < 10; j++)
             board[i][j] = '~';
+}
 
-//    board[2][3] = 'X'; 
-//    board[7][8] = 'O'; 
+// Print the board
+void printBoard(char board[10][10]) {
+    cout << "\n     0    1    2    3    4    5    6    7    8    9\n";
+    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n";
 
-    // Print column headers (hardcoded)
-    cout << "     0    1    2    3    4    5    6    7    8    9  " << endl;
+    for (int r = 0; r < 10; r++) {
+        cout << "  " << r << " ¦";
 
-    // Top border
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+" << endl;
+        for (int c = 0; c < 10; c++) {
+            char cell = board[r][c];
 
-    // Row 0
-    cout << "  0 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[0][i];
-        setColor(7, 1); cout << "  ~  "; setColor(7,0); cout << "¦";
+            if (cell == 'X') { setColor(WHITE, RED); }
+            else if (cell == 'O') { setColor(BLACK, GREEN); }
+            else { setColor(WHITE, BLUE); }
+
+            cout << "  " << cell << "  ";
+            setColor(WHITE, BLACK);
+            cout << "¦";
+        }
+
+        cout << "\n    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n";
     }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
+}
 
-    // Row 1
-    cout << "  1 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[1][i];
-        setColor(7, 1); cout << "  ~  "; setColor(7,0); cout << "¦";
+// Show Menu
+int showMenu() {
+    cout << "\n============== MAIN MENU ==============\n";
+    cout << "1. Start Game\n";
+    cout << "2. Instructions\n";
+    cout << "3. Show Board\n";
+    cout << "4. Exit\n";
+    cout << "=======================================\n";
+    cout << "Enter your choice: ";
+
+    int choice;
+    cin >> choice;
+
+    while (choice < 1 || choice > 4) {
+        cout << "Invalid choice. Try again: ";
+        cin >> choice;
     }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
-
-    // Row 2
-    cout << "  2 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[2][i];
-        if (cell == 'X') { setColor(15, 12); cout << "  X  "; setColor(7,0); }
-        else { setColor(7, 1); cout << "  ~  "; setColor(7,0); }
-        cout << "¦";
-    }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
-
-    // Row 3
-    cout << "  3 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[3][i];
-        setColor(7, 1); cout << "  ~  "; setColor(7,0); cout << "¦";
-    }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
-
-    // Row 4
-    cout << "  4 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[4][i];
-        setColor(7, 1); cout << "  ~  "; setColor(7,0); cout << "¦";
-    }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
-
-    // Row 5
-    cout << "  5 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[5][i];
-        setColor(7, 1); cout << "  ~  "; setColor(7,0); cout << "¦";
-    }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
-
-    // Row 6
-    cout << "  6 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[6][i];
-        setColor(7, 1); cout << "  ~  "; setColor(7,0); cout << "¦";
-    }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
-
-    // Row 7
-    cout << "  7 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[7][i];
-        if (cell == 'O') { setColor(0, 8); cout << "  O  "; setColor(7,0); }
-        else { setColor(7, 1); cout << "  ~  "; setColor(7,0); }
-        cout << "¦";
-    }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
-
-    // Row 8
-    cout << "  8 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[8][i];
-        setColor(7, 1); cout << "  ~  "; setColor(7,0); cout << "¦";
-    }
-    cout << endl;
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----¦" << endl;
-
-    // Row 9
-    cout << "  9 ¦"; 
-    for (int i = 0; i < 10; i++)
-    {
-        char cell = board[9][i];
-        setColor(7, 1); cout << "  ~  "; setColor(7,0); cout << "¦";
-    }
-    cout << endl;
-
-    // Bottom border
-    cout << "    +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+" << endl;
+    return choice;
 }
 
